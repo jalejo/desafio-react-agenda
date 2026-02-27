17e9b1f8e10b224058933998e2f1b90ce9d68340
Jose Alejo <ja.alejo@gmail.com>
2026-02-26 23:55:46 -0300

    test: add matchMedia polyfill for antd in jest

--- contact-app/src/setupTests.ts
+++ contact-app/src/setupTests.ts
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Ant Design / rc-util expects matchMedia in the browser.
+// JSDOM doesn't provide it, so we polyfill it for tests.
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
